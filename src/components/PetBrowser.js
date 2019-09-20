a019d26181fae10b4104e3eaeed159afb726e7b8
Brian <briandsalemi@gmail.com>
2019-09-20 19:20:13 -0400

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,8 +3,11 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
+  constructor(props){
+    super(props)
+  }
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return <div className="ui cards"><Pet pets={this.props.pets}/></div>
   }
 }
 
